5ad815bac15e2150bc4c49991e8f41051f42adb9
HaoMEI423 <s4164444@student.rmit.edu.au>
2026-07-29 15:15:07 +1000

    init project
    add basic file, linked to index, add a sound button

---
+++ script.js
@@ -0,0 +1,14 @@
+//find the button
+const testButton = document.getElementById('test-button')
+
+//init our synth
+const synth = new Toue.Synth().toDestination();
+
+//do something when button was clicked
+testButton.addEventListener("click", playNote);
+
+//function that runs when button is clicked
+function playNote(){
+    //play a note for a duration
+    synth.triggerAttackRelease("C4", "8n");
+}
