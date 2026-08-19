5860c5e801b3e5d3b3df5441617e64fbc8a230e7
HaoMEI423 <s4164444@student.rmit.edu.au>
2026-08-19 16:01:39 +1000

    add js and css

--- script.js
+++ script.js
@@ -8,9 +8,21 @@ const introDialogCloseButton = document.getElementById("intro-dialog-close")
 // console.log(introDialog);
 //find the button
 const testButton = document.getElementById('test-button');
-
+// find my key button
+const key = document.getElementById("key-test")
+// 
 //init our synth
-const synth = new Tone.Synth().toDestination();
+// change this to ply synth
+const synth = new Tone.PolySynth();
+// is the user currently holding down the key
+let mousebuttonHeld = false;
+// if user holds down key, set to true, then if they let it up, set to false
+window.addEventListener("mousedown", function(){
+    mouseButtonHeld = true;
+});
+window.addEventListener("mouseup", function(){
+    mouseButtonHeld = false;
+});
 
 ////// Dialog
 // show dialog on page
@@ -41,3 +53,38 @@ function playNote(){
     //play a note for a duration
     synth.triggerAttackRelease("C4", "8n");
 }
+
+function playDataNoteD(e){
+    let buttonClicked = e.target;
+    // console.log(buttonClicked);
+    let note = buttonClicked.dataset.note;
+    // console.log(e.target);
+    synth.triggerAttackRelease("d4", "8n");
+}
+
+function startNote(e){
+    // find key that was pressed
+    let keyPressed = e.target;
+    // find the note associated with the key
+    let note = keyPressed.dataset.note;
+    synth.triggerAttack(note);
+}
+
+function endNote(){
+    let keyPressed= e.target;
+    let note = keyPressed.dataset.note;
+    synth.triggerAttackRelease(note);
+}
+
+key.addEventListener("mousedown", startNote);
+key.addEventListener("mouseup", endNote);
+key.addEventListener("mouseleave", endNote);
+// if user is holding mouse button down when entering the key, play note
+key.addEventListener("mouseenter", function(e){
+    if (mouseButtonHeld === true) {
+        startNote(e);
+    }
+})
+
+// key.addEventListener("click", playDataNote);
+// testButton.addEventListener("click",playDataNote);
